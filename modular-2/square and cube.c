7e4e4b191ac4f42a7a342a024bb60bf26101ce6f
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2022-11-14 13:28:28 +0530

    Add files via upload
    Signed-off-by: rutvik1013 <113574475+rutvik1013@users.noreply.github.com>

---
+++ modular-2/square and cube.c
@@ -0,0 +1,11 @@
+//write a program to make a square and cube of the number
+#include<stdio.h>
+void main()
+{
+	int a,square,cube;
+	printf("Enter the number:");
+	scanf("%d",&a);
+	square=a*a;
+	cube=a*a*a;
+	printf("\n square is %d\n cube is %d",square,cube);
+}
