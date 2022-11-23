ba32a87eda99cbc75abf171101aacd7ecf149a20
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2022-11-23 11:53:32 +0530

    Add files via upload
    Signed-off-by: rutvik1013 <113574475+rutvik1013@users.noreply.github.com>

---
+++ Modular-5/1st.cpp
@@ -0,0 +1,9 @@
+// Write a c++ program t print Massage "Welcome to Tops Technologies".
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Welcome to Tops Technologies";
+	return 0;
+}
